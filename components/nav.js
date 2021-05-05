f4d508a9ced94a2234e23c547012c58ccbc97bc9
gOps132 <epilred@icloud.com>
2021-05-05 18:35:31 +0800

    Added svgr to make things simple

--- components/nav.js
+++ components/nav.js
@@ -3,6 +3,9 @@ import React from "react";
 
 import Link from "next/link";
 
+import HouseIcon from "../public/svg/house-user-solid.svg";
+import HandPeaceIcon from "../public/svg/hand-peace-solid.svg";
+
 const Nav = ({ theme, theme_callback, theme_map }) => {
 	const GetThemeType = () => {
 		if(theme == "dark")
@@ -35,14 +38,17 @@ const Nav = ({ theme, theme_callback, theme_map }) => {
 				</li>
 
 				<li className={nav_styles.nav_item}>
+					<Link href="/"><a className={nav_styles.nav_link}>
+						<HouseIcon/>
+					<span className={nav_styles.link_text}>Home</span>
+					</a>
+					</Link>
+				</li>
+				
+				<li className={nav_styles.nav_item}>
 					<Link href="/hello"><a className={nav_styles.nav_link}>
-						<svg aria-hidden="true" focusable="false" data-prefix="fas"
-							data-icon="headphones-alt" className="svg-inline--fa fa-headphones-alt fa-w-16" role="img"
-							xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-							<path fill="currentColor"
-								d="M160 288h-16c-35.35 0-64 28.7-64 64.12v63.76c0 35.41 28.65 64.12 64 64.12h16c17.67 0 32-14.36 32-32.06V320.06c0-17.71-14.33-32.06-32-32.06zm208 0h-16c-17.67 0-32 14.35-32 32.06v127.88c0 17.7 14.33 32.06 32 32.06h16c35.35 0 64-28.71 64-64.12v-63.76c0-35.41-28.65-64.12-64-64.12zM256 32C112.91 32 4.57 151.13 0 288v112c0 8.84 7.16 16 16 16h16c8.84 0 16-7.16 16-16V288c0-114.67 93.33-207.8 208-207.82 114.67.02 208 93.15 208 207.82v112c0 8.84 7.16 16 16 16h16c8.84 0 16-7.16 16-16V288C507.43 151.13 399.09 32 256 32z">
-							</path>
-						</svg> <span className={nav_styles.link_text}>Hello</span>
+						<HandPeaceIcon/>
+						<span className={nav_styles.link_text}>Hello</span>
 					</a>
 					</Link>
 				</li>
