966c47811ee28090236a8a57623097d99d8b901c
gOps132 <epilred@icloud.com>
2021-05-07 00:11:12 +0800

    staticprops for meme page working, data bugs must fix

--- pages/memes.js
+++ pages/memes.js
@@ -7,33 +7,42 @@ const Memes = (props) => {
 		<div>
 			<h1>This is the memes page</h1>
 			<div>
-				{props.main_obj.filename.map(i => {
-					return (
-						<img key={i} src={`/img/memes/${i}`} />
-					);
-				})}
-				{console.log(props.main_obj)}
+				{
+					props.main_obj.files.map(i => {
+						console.log(`filename: /img/memes/${i.filename}, width: ${i.width}, height: ${i.height}`);
+
+						return <img key={i.filename} src={`/img/memes/${i.filename}`} />
+					})
+				}
+				{/* {console.log(props.main_obj)} */}
 			</div>
 		</div>
 	);
 }
 
 export async function getStaticProps() {
-	let img_obj  = {
-		filename: [],
-		width: [],
-		height: [],
+	let img_obj = {
+		files: [],
 	}
-	img_obj.filename = fs.readdirSync("public/img/memes");
-	for(let i = 0; i < img_obj.filename.length; i++) {
-		sizeOf(`public/img/memes/${img_obj.filename[i]}`, (err, dimensions) => {
-			console.log(`dimensions of ${i}: ` + dimensions.width, dimensions.height);
-			// img_obj.width.append(dimensions.width);
-			// img_obj.height.append(dimensions.height);
+	let image_names = fs.readdirSync("public/img/memes");
+	for (let i = 0; i < image_names.length; i++) {
+		img_obj.files.push({
+			filename: image_names[i],
+			width: 0,
+			height: 0,
 		});
-		console.log(i);
 	}
-	console.log("files: " + img_obj.filename);
+	for (let i = 0; i < image_names.length; i++) {
+		try {
+			sizeOf(`public/img/memes/${img_obj.files[i].filename}`, (err, dimensions) => {
+				console.log(`dimensions of ${img_obj.files[i].filename}: ` + `width: ${dimensions.width}`, `height: ${dimensions.height}`);
+				img_obj.files[i].width = (dimensions.width);
+				img_obj.files[i].height = (dimensions.height);
+			});
+			console.log(i);
+		} catch (e) { throw e; }
+	}
+	// img_obj.files.map(i => console.log(i.filename))
 	return {
 		props: {
 			main_obj: img_obj
