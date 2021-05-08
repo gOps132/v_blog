2742a000906b0b452eaee34639a18c4d7d59de3e
Gian Cedrick G. Epilan <epilred@icloud.com>
2021-05-08 22:30:04 +0800

    Added width and height to getStaticProps images

--- pages/memes.js
+++ pages/memes.js
@@ -1,10 +1,9 @@
 import React from "react";
 const fs = require("fs");
 
-// TODO: delete
-// const sizeOf = require("image-size");
-// const gm = require('gm').subClass({imageMagick: true});;
-// var im = require('imagemagick');
+import Image from "next/image";
+
+const sizeOf = require('image-size');
 
 const Memes = (props) => {
 	return (
@@ -12,7 +11,13 @@ const Memes = (props) => {
 			<h1>This is the memes page</h1>
 			<div>
 				{props.main_obj.files.map(i => {
-					return <img key={i.filename} src={`/img/memes/${i.filename}`} />
+					return <Image
+						key={i.filename}
+						width={i.width}
+						height={i.height}
+						src={`/img/memes/${i.filename}}`} 
+						layout="responsive"
+					/>
 				})}
 				{console.log(props.main_obj)}
 			</div>
@@ -20,65 +25,21 @@ const Memes = (props) => {
 	);
 }
 
-export function getStaticProps() {
+export async function getStaticProps() {
 	let img_obj = {
 		files: [],
 	}
+
 	let image_names = fs.readdirSync("public/img/memes");
 	for (let i = 0; i < image_names.length; i++) {
 		img_obj.files.push({
 			filename: image_names[i],
-			width: 0,
-			height: 0
+			width: sizeOf(`public/img/memes/${image_names[i]}`).width,
+			height: sizeOf(`public/img/memes/${image_names[i]}`).height,
 		});
 	}
 
-	{
-		// let width = sizeOf(`../public/img/memes/${image_names[1]}`, (e, d) => { return parseInt(d.width)});
-		// let height = sizeOf(`../public/img/memes/${image_names[1]}`, (e, d) => { return parseInt(d.height)});
-		// console.log(width, height);
-
-		// FIXME: This does not change the object value
-		// for (let i = 0; i < img_obj.files.length; i++) {
-		// 	let width, height;
-		// 	sizeOf(`public/img/memes/${img_obj.files[i].filename}`, (err, dimensions) => {
-		// 		try {
-		// 			if (!err) {
-		// 				let image_dimensions = dimensions || "";
-		// 				width = image_dimensions.width;
-		// 				height = image_dimensions.height;
-
-		// 				// console.log(`dimensions of ${img_obj.files[i].filename}: ` + `width: ${width}`, `height: ${height}`);
-		// 				// img_obj.files[i].width = width;
-		// 				// img_obj.files[i].height = height;
-		// 			}
-		// 		} catch (e) {
-		// 			throw (e)
-		// 		}
-		// 	});
-		// 	console.log(width, height);
-		// }
-
-		// img_obj.files[0].width = 584;
-		// img_obj.files[0].height = 427;
-
-		// img_obj.files.map(i => console.log(i.filename))
-	}
-
-	for (let i = 0; i < img_obj.files.length; i++) {
-		// gm(`img/memes/${img_obj.files[i].filename}`).size( (err, size) => {
-		// 	if (!err) {
-		// 		console.log('width: ' + size.width);
-		// 		console.log('height: ' + size.height);
-		// 	}
-		// });
-		
-		// im.identify(['-format', '%wx%h',`/public/img/memes/${img_obj.files[i].filename}`], function(err, features){
-		// 	if (err) throw err;
-		// 	console.log("dimensions: " + features);
-		// 	// { format: 'JPEG', width: 3904, height: 2622, depth: 8 }
-		// })
-	}
+	console.log(`main_obj: ${img_obj.files[0].width}`);
 
 	return {
 		props: {
