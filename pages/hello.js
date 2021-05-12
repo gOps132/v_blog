9f58500ecfe9f82bf846d4de19b404c3db78b818
gOps132 <epilred@icloud.com>
2021-05-12 12:51:38 +0800

    did a lot of confusing hacks for this layout

--- pages/hello.js
+++ pages/hello.js
@@ -5,11 +5,11 @@ export default function Home() {
 		<div className={common_style.main_div}>
 			<h1>Hello World!</h1>
 			<div>
-				<p>This is a website that I remade the next.js framework, took some time to learn but it's worth it</p>
-				<p>The memes page is statically served</p>
-				<p>The music page gets contents from the spotify web API</p>
-				<p>The movie page gets contents from the imdb API</p>
-				<p>See the git respository for more info</p>
+				<p>This is a website that I remade the next.js framework, took some time to learn but it's worth it 
+					The memes page is statically served. The music page gets contents from the spotify web API. 
+					The movie page gets contents from the imdb API. See the git respository for more info
+				</p>
+				<p>NOTE: This is my first production implementation of an API</p>
 			</div>
 		</div>
 	);
--- pages/music.js
+++ pages/music.js
@@ -6,6 +6,7 @@ import Link from "next/link";
 import Image from "next/image";
 
 import { useRouter } from 'next/router'
+import { useState } from "react";
 
 import Loader from "../components/loader";
 
@@ -13,78 +14,94 @@ import Loader from "../components/loader";
 // TODO: add a loader for SSG load times
 
 const Music = (props) => {
-    const router = useRouter();
-    let src_set_str = "";
-    if (router.isFallback) {
-        return <Loader />
-    } else {
-        props.spotify_data.images.map(i => {
-            src_set_str += `${i.url} ${i.width}w, `;
-        })
-    }
-    return (
-        <div className={common_styles.main_div}>
-            {
-                <>
-                    {/* TODO: use the preview url to preview the song */}
-                    {console.log(props.spotify_data)}
-                    {
-                        (!props.err ?
-                            <div className={list_styles.list}>
-                                <div className={list_styles.play_list_header}>
-                                    {<Image
-                                        className={image_styles.image_border_circle}
-                                        src={props.spotify_data.images[0].url}
-                                        srcSet={src_set_str}
-                                        width={150}
-                                        height={150}
-                                    />}
-                                    <h1 className={list_styles.play_list_name}>{props.spotify_data.name}</h1>
-                                </div>
-                                <ul>
-                                    {props.spotify_data.tracks.items.map((i, t) => {
-                                        return (
-                                            <Link href={i.track.external_urls.spotify} key={t}><a>
-                                                <li className={list_styles.list_element}>
-                                                    <p className={list_styles.index}>{t + 1}</p>
-                                                    <div className={list_styles.container}>
-                                                        <p>{i.track.name}</p>
-                                                    </div>
-                                                    <></>
-                                                </li>
-                                            </a></Link>
-                                        )
-                                    })}
-                                </ul>
-                            </div>
-                            : <h1>Fetch or Server Error, reload for more info</h1>)
-                    }
-                </>
-            }
-        </div>
-    );
+	const router = useRouter();
+	let [isPlaying, useIsPlaying] = useState(false);
+	if (router.isFallback) {
+		return <Loader />
+	}
+	return (
+		<div className={common_styles.main_div}>
+			{
+				<>
+					{/* TODO: use the preview url to preview the song */}
+					{console.log(props.spotify_data)}
+					{
+						(!props.err ?
+							<div className={list_styles.list}>
+								<div className={list_styles.play_list_header}>
+									{<Image
+										className={image_styles.image_border_circle}
+										src={props.spotify_data.images[0].url}
+										width={150}
+										height={150}
+									/>}
+									<h1 className={list_styles.play_list_name}>{props.spotify_data.name}</h1>
+								</div>
+								<ul>
+									{props.spotify_data.tracks.items.map((i, t) => {
+										return (
+											// <Link href={i.track.external_urls.spotify} key={t}>
+											// <a>
+											<li className={list_styles.list_element}>
+												<p className={list_styles.index}>{t + 1}</p>
+												<Link href={i.track.external_urls.spotify} key={t}>
+													<div className={list_styles.container}>
+														<p>{i.track.name}</p>
+													</div>
+												</Link>
+												{
+													<>
+														{(i.track.preview_url) ?
+															<div className={list_styles.preview_button}>
+																<button 
+																	
+																	onClick={() => {
+																		let pid = document.getElementById(`preview-${t}`);
+																		(isPlaying ? pid.pause() : pid.play());
+																		useIsPlaying(!isPlaying);
+																	}}
+																>preview</button>
+																<audio id={`preview-${t}`} >
+																	<source src={i.track.preview_url} type="audio/mp3" />
+																</audio>
+															</div> : <div className={list_styles.preview_button}></div>}
+													</>
+												}
+											</li>
+											// </a>
+											// </Link>
+										)
+									})}
+								</ul>
+							</div>
+							: <h1>Fetch or Server Error, reload for more info</h1>)
+					}
+				</>
+			}
+		</div>
+	);
 }
 
 export async function getStaticProps() {
-    let res = await fetch(`${process.env.SERVER_URL}/api/spotify`, { method: "GET" })
+	let res = await fetch(`${process.env.SERVER_URL}/api/spotify`, { method: "GET" })
 
-    if (res.status === 204 || res.status > 400) {
-        return {
-            props: {
-                err: true
-            }
-        }
-    }
+	if (res.status === 204 || res.status > 400) {
+		return {
+			props: {
+				err: true
+			}
+		}
+	}
 
-    let data = await res.json();
+	let data = await res.json();
 
-    return {
-        props: {
-            spotify_data: data,
-            err: false
-        },
-        revalidate: 60
-    };
+	return {
+		props: {
+			spotify_data: data,
+			err: false
+		},
+		revalidate: 60
+	};
 };
 
 export default Music;
