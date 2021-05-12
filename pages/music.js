d612abd4ba533c11aa59ba5c5e94330b23d6b6c2
gOps132 <epilred@icloud.com>
2021-05-12 16:53:40 +0800

    Added temp preview button

--- pages/music.js
+++ pages/music.js
@@ -10,86 +10,82 @@ import { useState } from "react";
 
 import Loader from "../components/loader";
 
+import PlayCircle from "../public/svg/play-circle-solid.svg";
+
 // TODO: add a server error guard
 // TODO: add a loader for SSG load times
 
 const Music = (props) => {
 	const router = useRouter();
 	let [isPlaying, useIsPlaying] = useState(false);
-	let [isOver, useIsOver] = useState(false);
 	if (router.isFallback) {
 		return <Loader />
 	}
 	return (
 		<>
-			<div className={common_styles.main_div}>
-				<>
-					{console.log(props.spotify_data)}
-					{
-						(!props.err ?
-							<>
-								<div className={list_styles.play_list_header}>
-									{<Image
-										className={image_styles.image_border_circle}
-										src={props.spotify_data.images[0].url}
-										width={150}
-										height={150}
-									/>}
-									<h1 className={list_styles.play_list_name}>{props.spotify_data.name}</h1>
-								</div>
-								<div className={list_styles.list}>
-									<ul>
-										{props.spotify_data.tracks.items.map((i, t) => {
-											return (
-												<li className={list_styles.list_element}>
-													<div
-														// onMouseOver={()=>useIsOver(!isOver)}
-														// onMouseOut={()=>useIsOver(!isOver)}
-													>
-														{
-															// (isOver) ?
-															// 	<>
-															// 		{
-															// 			(i.track.preview_url) ?
-															// 				<div className={list_styles.preview_button}>
-															// 					<button
-															// 						onClick={() => {
-															// 							let pid = document.getElementById(`preview-${t}`);
-															// 							(isPlaying ? pid.pause() : pid.play());
-															// 							useIsPlaying(!isPlaying);
-															// 						}}
-															// 					>
-															// 						{/* make into play button svg */}
-															// 					preview
-															// 				</button>
-															// 				</div> :
-															// 				<div className={list_styles.preview_button}></div>
-															// 		}
-															// 	</> :
-																<p className={list_styles.index}>{t + 1}</p>
-														}
+			<div className={`${common_styles.main_div}`}>
+				{/* <> */}
+				{console.log(props.spotify_data)}
+				{
+					(!props.err ?
+						<>
+							<div className={list_styles.play_list_header}>
+								{<Image
+									className={image_styles.image_border_circle}
+									src={props.spotify_data.images[0].url}
+									width={150}
+									height={150}
+								/>}
+								<h1 className={list_styles.play_list_name}>{props.spotify_data.name}</h1>
+							</div>
+							<div className={list_styles.list}>
+								<ul>
+									{props.spotify_data.tracks.items.map((i, t) => {
+										return (
+											<li id={t} className={list_styles.list_element}>
+												<div>
+													{
+														<>
+															{
+																(i.track.preview_url) ?
+																	<div className={list_styles.preview_button}>
+																		{/* on play change to pause button */}
+																		<PlayCircle
+																			className={list_styles.play_button}
+																			onClick={() => {
+																				let pid = document.getElementById(`preview-${t}`);
+																				(isPlaying ? pid.pause() : pid.play());
+																				useIsPlaying(!isPlaying);
+																			}}
+																		/>
+																	</div>
+																	: <>NONE</>
+															}
+														</>
+													}
+												</div>
+
+												<audio id={`preview-${t}`} >
+													<source src={i.track.preview_url} type="audio/mp3" />
+												</audio>
+
+												<Link href={i.track.external_urls.spotify} key={t}>
+													<div className={list_styles.container}>
+														<u>{i.track.name}</u>
 													</div>
+												</Link>
 
-													<audio id={`preview-${t}`} >
-														<source src={i.track.preview_url} type="audio/mp3" />
-													</audio>
-
-													<Link href={i.track.external_urls.spotify} key={t}>
-														<div className={list_styles.container}>
-															<u>{i.track.name}</u>
-														</div>
-													</Link>
-
-												</li>
-											)
-										})}
-									</ul>
-								</div>
-							</>
-							: <h1>Fetch or Server Error, reload for more info</h1>
-						)
-					}
-				</>
+												<p className={list_styles.index}>{t + 1}</p>
+											</li>
+										)
+									})}
+								</ul>
+							</div>
+						</>
+						: <h1>Fetch or Server Error, reload for more info</h1>
+					)
+				}
+				{/* </> */}
 			</div>
 		</>
 	);
