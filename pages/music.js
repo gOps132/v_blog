b573cc2bf305ff212921c183c15025ed1ed0bfd0
gOps132 <epilred@icloud.com>
2021-05-12 16:26:58 +0800

    added more styling to music page

--- pages/music.js
+++ pages/music.js
@@ -16,18 +16,18 @@ import Loader from "../components/loader";
 const Music = (props) => {
 	const router = useRouter();
 	let [isPlaying, useIsPlaying] = useState(false);
+	let [isOver, useIsOver] = useState(false);
 	if (router.isFallback) {
 		return <Loader />
 	}
 	return (
-		<div className={common_styles.main_div}>
-			{
+		<>
+			<div className={common_styles.main_div}>
 				<>
-					{/* TODO: use the preview url to preview the song */}
 					{console.log(props.spotify_data)}
 					{
 						(!props.err ?
-							<div className={list_styles.list}>
+							<>
 								<div className={list_styles.play_list_header}>
 									{<Image
 										className={image_styles.image_border_circle}
@@ -37,47 +37,61 @@ const Music = (props) => {
 									/>}
 									<h1 className={list_styles.play_list_name}>{props.spotify_data.name}</h1>
 								</div>
-								<ul>
-									{props.spotify_data.tracks.items.map((i, t) => {
-										return (
-											// <Link href={i.track.external_urls.spotify} key={t}>
-											// <a>
-											<li className={list_styles.list_element}>
-												<p className={list_styles.index}>{t + 1}</p>
-												<Link href={i.track.external_urls.spotify} key={t}>
-													<div className={list_styles.container}>
-														<p>{i.track.name}</p>
+								<div className={list_styles.list}>
+									<ul>
+										{props.spotify_data.tracks.items.map((i, t) => {
+											return (
+												<li className={list_styles.list_element}>
+													<div
+														// onMouseOver={()=>useIsOver(!isOver)}
+														// onMouseOut={()=>useIsOver(!isOver)}
+													>
+														{
+															// (isOver) ?
+															// 	<>
+															// 		{
+															// 			(i.track.preview_url) ?
+															// 				<div className={list_styles.preview_button}>
+															// 					<button
+															// 						onClick={() => {
+															// 							let pid = document.getElementById(`preview-${t}`);
+															// 							(isPlaying ? pid.pause() : pid.play());
+															// 							useIsPlaying(!isPlaying);
+															// 						}}
+															// 					>
+															// 						{/* make into play button svg */}
+															// 					preview
+															// 				</button>
+															// 				</div> :
+															// 				<div className={list_styles.preview_button}></div>
+															// 		}
+															// 	</> :
+																<p className={list_styles.index}>{t + 1}</p>
+														}
 													</div>
-												</Link>
-												{
-													<>
-														{(i.track.preview_url) ?
-															<div className={list_styles.preview_button}>
-																<button 
-																	onClick={() => {
-																		let pid = document.getElementById(`preview-${t}`);
-																		(isPlaying ? pid.pause() : pid.play());
-																		useIsPlaying(!isPlaying);
-																	}}
-																>preview</button>
-																<audio id={`preview-${t}`} >
-																	<source src={i.track.preview_url} type="audio/mp3" />
-																</audio>
-															</div> : <div className={list_styles.preview_button}></div>}
-													</>
-												}
-											</li>
-											// </a>
-											// </Link>
-										)
-									})}
-								</ul>
-							</div>
-							: <h1>Fetch or Server Error, reload for more info</h1>)
+
+													<audio id={`preview-${t}`} >
+														<source src={i.track.preview_url} type="audio/mp3" />
+													</audio>
+
+													<Link href={i.track.external_urls.spotify} key={t}>
+														<div className={list_styles.container}>
+															<u>{i.track.name}</u>
+														</div>
+													</Link>
+
+												</li>
+											)
+										})}
+									</ul>
+								</div>
+							</>
+							: <h1>Fetch or Server Error, reload for more info</h1>
+						)
 					}
 				</>
-			}
-		</div>
+			</div>
+		</>
 	);
 }
 
