6425edfbf3a47189d4d53e49128a3c7a12f7594e
gOps132 <epilred@icloud.com>
2021-05-13 20:04:25 +0800

    divided favicon

--- pages/hello.js
+++ pages/hello.js
@@ -5,10 +5,12 @@ export default function Home() {
 		<div className={common_style.main_div}>
 			<h1>Hello World!</h1>
 			<div>
-				<p>This is a website that I remade the next.js framework, took some time to learn but it's worth it 
+				<p>
+					This is a website that I remade the next.js framework, took some time to learn but it's worth it 
 					The memes page is statically served. The music page gets contents from the spotify web API. 
 					The movie page gets contents from the imdb API. See the git respository for more info
 				</p>
+				<p>I optimized a bunch of stuff for production, so this should be fast when cached</p>
 				<p>NOTE: This is my first production implementation of an API</p>
 			</div>
 		</div>
